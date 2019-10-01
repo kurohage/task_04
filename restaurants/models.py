1cfb22c8bea7a2d6e66bd45b47bcb4cf50528b1e
kurohage <517326+kurohage@users.noreply.github.com>
2019-10-01 19:39:07 +0300

    Modified model field to time fields instead of limited char fields.

--- restaurants/models.py
+++ restaurants/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Restaurant(models.Model):
 	name = models.CharField(max_length=50)
 	description = models.CharField(max_length=60)
-	opening_time = models.CharField(max_length=5)
-	closing_time = models.CharField(max_length=5)
+	opening_time = models.TimeField()
+	closing_time = models.TimeField()
 
 	def __str__(self):
 		return self.name
