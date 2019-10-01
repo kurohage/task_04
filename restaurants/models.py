c7919d0e297c67774d00baaaec445838c6e99ce1
kurohage <517326+kurohage@users.noreply.github.com>
2019-10-01 18:48:46 +0300

    created Restaurant mode

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=60)
+	opening_time = models.CharField(max_length=5)
+	closing_time = models.CharField(max_length=5)
+
+	def __str__(self):
+		return self.name
